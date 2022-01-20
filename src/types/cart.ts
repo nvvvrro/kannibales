18419310929fa72a624c68041e308a256651a21e
Marcelo Navarro <84986697+nvvvrro@users.noreply.github.com>
2022-01-20 12:26:44 -0300

    fix LineItems: added Items types

---
+++ src/types/cart.ts
@@ -0,0 +1,11 @@
+export type LineItem = {
+  id: string;
+  variantId: string;
+  productId: string;
+  name: string;
+  quantity: number;
+  path: string;
+  value: string;
+  price: number;
+  image: string;
+};
