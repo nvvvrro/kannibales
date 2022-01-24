e2a438d06fb73e78f8b51b3cbd1875d50ecc44a8
Marcelo Navarro <84986697+nvvvrro@users.noreply.github.com>
2022-01-24 15:29:10 -0300

    fix: added MenuProfile component

---
+++ src/components/structural/MenuProfile.tsx
@@ -0,0 +1,55 @@
+export const MenuProfile = () => {
+  return (
+    <div className="ml-3 relative">
+      <div>
+        <button
+          type="button"
+          className="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+          id="user-menu-button"
+          aria-expanded="false"
+          aria-haspopup="true"
+        >
+          <span className="sr-only">Open user menu</span>
+
+          <img
+            className="h-8 w-8 rounded-full"
+            src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+            alt=""
+          />
+        </button>
+      </div>
+
+      <div
+        className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
+        role="menu"
+        aria-orientation="vertical"
+        aria-labelledby="user-menu-button"
+      >
+        <a
+          href="#"
+          className="block px-4 py-2 text-sm text-gray-700"
+          role="menuitem"
+          id="user-menu-item-0"
+        >
+          Your Profile
+        </a>
+        <a
+          href="#"
+          className="block px-4 py-2 text-sm text-gray-700"
+          role="menuitem"
+          id="user-menu-item-1"
+        >
+          Settings
+        </a>
+        <a
+          href="#"
+          className="block px-4 py-2 text-sm text-gray-700"
+          role="menuitem"
+          id="user-menu-item-2"
+        >
+          Sign out
+        </a>
+      </div>
+    </div>
+  );
+};
--- src/components/structural/index.ts
+++ src/components/structural/index.ts
@@ -2,3 +2,4 @@ export * from "./Header";
 export * from "./Footer";
 export * from "./FooterBottom";
 export * from "./Layout";
+export * from "./MenuProfile";
