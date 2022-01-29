50e24df13baf338c38b5e8c778d78916178d641f
Marcelo Navarro <84986697+nvvvrro@users.noreply.github.com>
2022-01-29 18:15:46 -0300

    feat: added social networks icons

--- src/images/icons/index.ts
+++ src/images/icons/index.ts
@@ -3,3 +3,5 @@ export * from "./IncognitoIcon";
 export * from "./LupaIcon";
 export * from "./TagIcon";
 export * from "./GoogleIcon";
+export * from "./FacebookIcon";
+export * from "./TwitterIcon";
