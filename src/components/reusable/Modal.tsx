d2425d0529a917a2a24b8512307a707ab28c0368
Marcelo Navarro <84986697+nvvvrro@users.noreply.github.com>
2022-01-31 01:17:04 -0300

    fix: background style Modal

--- src/components/reusable/Modal.tsx
+++ src/components/reusable/Modal.tsx
@@ -11,9 +11,7 @@ export const Modal: FC<Props> = ({ children, className }) => {
   return (
     <div
       id="modal-bg"
-      className={`${
-        className || ""
-      }  h-full w-full py-16 px-4 absolute inset-0 bg-gradient-to-tl from-primary-light to-primary-dark opacity-75 top-0 min-w-0 z-10`}
+      className={`${className || ""} h-full w-full py-16 px-4 ease-in-out absolute inset-0 bg-primary-dark bg-opacity-60 top-0 min-w-0 z-10`}
     >
       <div className="flex flex-col items-center justify-center">
         <div
@@ -28,7 +26,7 @@ export const Modal: FC<Props> = ({ children, className }) => {
                 document.getElementById("modal-bg")?.remove();
               }}
             >
-              <XIcon className="w-8 h-8 " />
+              <XIcon className="w-8 h-8" />
             </Button>
           </div>
 
