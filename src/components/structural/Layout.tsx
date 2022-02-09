891f5fa3b04e627e97e499662fcd1c135e612455
Marcelo Navarro <84986697+nvvvrro@users.noreply.github.com>
2022-02-08 23:57:08 -0300

    feat: added first work of sidebar

--- src/components/structural/Layout.tsx
+++ src/components/structural/Layout.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Footer, Header } from ".";
+import { Sidebar } from "components/sidebar";
 
 interface Props {
   children: React.ReactNode;
@@ -10,7 +11,10 @@ interface Props {
 export const Layout = ({ children, className, pathName }: Props) => {
   return (
     <div className={className || ""}>
-      <Header />
+      <Header>
+        <Sidebar />
+      </Header>
+
       {children}
       <Footer />
     </div>
