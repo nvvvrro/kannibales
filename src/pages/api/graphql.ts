0ed27d535759b2ca192878fb7f07270b53d3d2c9
Marcelo Navarro <84986697+nvvvrro@users.noreply.github.com>
2022-03-09 23:59:18 -0300

    fix: added gql

--- src/pages/api/graphql.ts
+++ src/pages/api/graphql.ts
@@ -1,24 +1,38 @@
 import { ApolloServer, gql } from "apollo-server-micro";
+import { NextApiRequest, NextApiResponse } from "next";
+import typeDefs from "graphql/typeDefs";
+import resolvers from "graphql/resolvers";
 
-const typeDefs = gql`
-  type Query {
-    hello: String
+const apolloServer = new ApolloServer({ typeDefs, resolvers });
+
+const startServer = apolloServer.start();
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://studio.apollographql.com"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  if (req.method === "OPTIONS") {
+    res.end();
+    return false;
   }
-`;
 
-const resolvers = {
-  Query: {
-    hello: () => "Hello world!",
-  },
-};
+  await startServer;
+  await apolloServer.createHandler({
+    path: "/api/graphql",
+  })(req, res);
+}
 
 export const config = {
   api: {
     bodyParser: false,
   },
 };
-
-const apolloServer = new ApolloServer({ typeDefs, resolvers });
-module.exports = apolloServer.start().then(() => {
-  return apolloServer.createHandler({ path: "/api/graphql" });
-});
